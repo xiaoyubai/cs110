eacb1d6f1ffadb2da5ef81e393ac96535a2f3105
Edmund Mok <edmundmok@outlook.com>
2018-07-26 12:39:22 +0800

    assign2: Revert to starter

--- assign/assign2 - Reading Unix v6 File Systems/file.c
+++ assign/assign2 - Reading Unix v6 File Systems/file.c
@@ -5,31 +5,8 @@
 #include "inode.h"
 #include "diskimg.h"
 
-/**
- * Fetches the specified file block from the specified inode.
- * Returns the number of valid bytes in the block, -1 on error.
- */
+// remove the placeholder implementation and replace with your own
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-	// get inode content
-	struct inode my_inode;
-	int err = inode_iget(fs, inumber, &my_inode);
-	if(err < 0) return -1;
-
-	// get true block num
-	int sector = inode_indexlookup(fs, &my_inode, blockNum);
-	if(sector < 0) return -1;
-
-	// get block content
-	int read_err = diskimg_readsector(fs->dfd, sector, buf);
-	if(read_err < 0) return -1;
-
-	// get bytes and blocks
-	int total_bytes = inode_getsize(&my_inode);
-	if(total_bytes < 0) return -1;
-	int total_blocks = total_bytes / DISKIMG_SECTOR_SIZE;
-	if(blockNum == total_blocks) {
-		return total_bytes % DISKIMG_SECTOR_SIZE;
-	} else {
-		return DISKIMG_SECTOR_SIZE;
-	}
+  fprintf(stderr, "file_getblock(inumber = %d, blockNum = %d) unimplemented. returning -1\n", inumber, blockNum);
+  return -1;  
 }
