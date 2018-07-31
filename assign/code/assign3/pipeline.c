7b67bb0065599bbfaf3ce98ed8e9f8a2690bc7f9
Edmund Mok <edmundmok@outlook.com>
2018-07-31 17:26:10 +0800

    assign3: Add basic implementation for task1

--- assign/code/assign3/pipeline.c
+++ assign/code/assign3/pipeline.c
@@ -10,5 +10,51 @@
 #include <errno.h>
 
 void pipeline(char *argv1[], char *argv2[], pid_t pids[]) {
-  fprintf(stderr, "pipeline: not yet implemented.\n");
+  // fds[0] - read, fds[1] - write
+  int fds[2];
+  pipe(fds);
+
+  pid_t reader = fork();
+  if (reader == -1) {
+    fprintf(stderr, "Failed to create reader process.\n");
+    return;
+  }
+
+  if (!reader) {
+    // In child reader process
+    // close write end of pipe
+    close(fds[1]);
+
+    // exec argv2 using fds[0] as stdin
+    dup2(fds[0], STDIN_FILENO);
+    execvp(argv2[0], argv2);
+  }
+
+  // In parent pipeline process
+  pids[1] = reader;
+
+  // close fds[0] since child reader already has it
+  close(fds[0]);
+
+  pid_t writer = fork();
+  if (writer == -1) {
+    fprintf(stderr, "Failed to create writer process.\n");
+    return;
+  }
+
+  if (!writer) {
+    // In child writer process
+    // close read end of pipe
+    close(fds[0]);
+
+    // exec argv1 using fds[1] as stdout
+    dup2(fds[1], STDOUT_FILENO);
+    execvp(argv1[0], argv1);
+  }
+
+  // close fds[1] since child writer already has it
+  close(fds[1]);
+
+  // In parent pipeline process again
+  pids[0] = writer;
 }
