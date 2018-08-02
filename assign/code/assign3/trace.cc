4624e3f87a809b618fa740b3c8e0928485e49890
Edmund Mok <edmundmok@outlook.com>
2018-08-02 13:28:47 +0800

    assign3: Add partial implementation for trace.cc

--- assign/code/assign3/trace.cc
+++ assign/code/assign3/trace.cc
@@ -15,6 +15,7 @@
 #include <map>
 #include <set>
 #include <unistd.h> // for fork, execvp
+#include <signal.h>
 #include <string.h> // for memchr, strerror
 #include <sys/ptrace.h>
 #include <sys/reg.h>
@@ -25,6 +26,8 @@
 #include "trace-exception.h"
 using namespace std;
 
+#define SIGTRAP2 (SIGTRAP | 0x80)
+
 int main(int argc, char *argv[]) {
   bool simple = false, rebuild = false;
   int numFlags = processCommandLineFlags(simple, rebuild, argv);
@@ -32,6 +35,48 @@ int main(int argc, char *argv[]) {
     cout << "Nothing to trace... exiting." << endl;
     return 0;
   }
+
+  int trueArgvIndex = numFlags+1;
+  map<int, std::string> systemCallNumbers;
+  map<std::string, int> systemCallNames;
+  map<std::string, systemCallSignature> systemCallSignatures;
+  compileSystemCallData(systemCallNumbers, systemCallNames, systemCallSignatures, rebuild);
+
+  int pid = fork();
+  if (pid < 0) {
+    fprintf(stderr, "Failed to fork.\n");
+    return 1;
+  }
+
+  if (pid == 0) {
+    ptrace(PTRACE_TRACEME);
+    raise(SIGSTOP);
+    execvp(argv[trueArgvIndex], argv+trueArgvIndex);
+  } else {
+    waitpid(pid, NULL, 0);
+    ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+    ptrace(PTRACE_SYSCALL, pid, 0, 0);
+    int status;
+    while (true) {
+      waitpid(pid, &status, 0);
+      if (WIFEXITED(status)) {
+        break;
+      } else if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP2) {
+        int syscall = ptrace(PTRACE_PEEKUSER, pid, ORIG_RAX * sizeof(long));
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
+        waitpid(pid, NULL, 0);
+        long retval = ptrace(PTRACE_PEEKUSER, pid, RAX * sizeof(long));
+        cout << "syscall(" << syscall << ") = " << retval << endl;
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
+      } else {
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
+      }
+    }
+
+    cout << "Program exited normally with status " << WEXITSTATUS(status) << endl;
+    return WEXITSTATUS(status);
+  }
+
 
   return 0;
 }
