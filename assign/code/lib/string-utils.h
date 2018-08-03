ad9f963e3fffbb1fbe5e5b19855aff334889806f
Edmund Mok <edmundmok@outlook.com>
2018-08-03 09:30:34 +0800

    assign4: Add lib folder

---
+++ assign/code/lib/string-utils.h
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static string join(const vector<string>& strs, const string& delim) {
+  string res;
+  if (strs.empty()) return "";
+  res += strs[0];
+  for (size_t i=1; i<strs.size(); i++) {
+    res += delim;
+    res += strs[i];
+  }
+  return res;
+}
+
+static bool startsWith(const string& a, const string& b) {
+  if (a.length() < b.length()) return false;
+  for (size_t i=0; i<b.length(); i++)
+    if (a[i] != b[i])
+      return false;
+  return true;
+}
+
+static const string trim(const string& s) {
+  size_t start, end, len;
+  for (start=0; start<s.length(); start++) {
+    if (s[start] != ' ') break;
+  }
+  for (end=s.length()-1; end>=0; end--) {
+    if (s[end] != ' ') break;
+  }
+  len = end-start+1;
+  string res = s.substr(start, len);
+  return res;
+}
