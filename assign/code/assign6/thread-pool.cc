d924834a8c581f2d0c3b0d9961958a025b95501b
Edmund Mok <edmundmok@outlook.com>
2018-08-08 20:01:46 +0800

    assign6: ThreadPool placeholders

--- assign/code/assign6/thread-pool.cc
+++ assign/code/assign6/thread-pool.cc
@@ -7,7 +7,17 @@
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads) {}
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads) {
+  dt = thread([this](){
+    dispatcher();
+  });
+
+  for (size_t workerID = 0; workerID < numThreads; workerID++) {
+    wts[workerID] = thread([this](size_t workerID){
+      worker(workerID);
+    }, workerID);
+  }
+}
 void ThreadPool::schedule(const function<void(void)>& thunk) {}
 void ThreadPool::wait() {}
 ThreadPool::~ThreadPool() {}
